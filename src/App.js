d5dd7e9285181fbec803a1a411bb4e6b789bad69
gurpreet0909 <gurpreet_babloo@yahoo.co.in>
2020-12-24 10:52:30 -0500

    Added products

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 // feature-1
 import React from 'react';
+import data from "./data.json";
+import Product from './compoenets/Product';
 
-function App() {
-  return (
-    <div class="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-          Product List
-      </main>
-      <footer>
-         All rights reserved.
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+
+  render() {
+    return (
+      <div class="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+            <div className= "content">
+              <div className="main">
+                <Product products={this.state.products}></Product>
+              </div>
+              <div className="sidebar">
+              cart items
+              </div>
+            </div>
+        </main>
+        <footer>
+           All rights reserved.
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
