fed0c238dcb73be3850adaedcf69b608545f5668
gurpreet0909 <gurpreet_babloo@yahoo.co.in>
2020-12-26 18:24:52 -0500

    final commit

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { productsReducer } from './reducers/productReduces';
 import { cartReducer } from './reducers/cartReducer';
+import { orderReducer } from './reducers/orderReducer';
 
 const initialState = {};
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -9,6 +10,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(combineReducers({
   products: productsReducer,
   cart: cartReducer,
+  order: orderReducer
 }),
   initialState,
   composeEnhancers(applyMiddleware(thunk))
